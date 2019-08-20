d0eb4050727acee09c57990cc777291b62e5df4a
Alper-Enes-DURU <enesduru@gmail.com>
2019-08-20 13:16:56 +0000

    Code completed. Output is same as in the lesson.

--- image_blur_1.rb
+++ image_blur_1.rb
@@ -1,1 +1,26 @@
-puts "Hello world"
+class Image
+  def initialize(arr)
+    @array = []
+    @array.push(arr)
+  end
+
+  def output_image
+    for j in @array do 
+      for k in j do
+        for l in k do
+          print l
+        end
+        puts "\n"
+      end
+    end
+  end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output_image
